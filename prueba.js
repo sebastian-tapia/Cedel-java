7799b1bf90ac5ce025cead6df5e03d8cdc4d4180
tapia <sebastian.tapia35202@gmail.com>
2022-07-27 17:59:34 -0300

    agregamos todos los archivos de la carpeta

---
+++ prueba.js
@@ -0,0 +1,13 @@
+function suma(a,b) {
+    return a+b
+    
+}
+
+console.log(suma(1,2))
+
+
+function dobleDsuma(a,b){
+    return suma(a,b)*2
+}
+
+console.log(dobleDsuma(3,2))
---
+++ ssss.js
@@ -0,0 +1,8 @@
+var uno=0
+var dos=3
+function asd(uno,dos) {
+    while (uno<dos) {
+        return "hola"
+    }
+}
+console.log(asd)
